bf6c63a83ec233f02d05073a186f83bc5687550b
mark-solo <clueless.magician@gmail.com>
2019-01-12 21:20:42 +0300

    display all posts

--- app/blog.py
+++ app/blog.py
@@ -2,8 +2,16 @@ from flask import (
 	Blueprint, render_template
 )
 
+from app.db import get_db
+
 bp = Blueprint('blog', __name__)
 
 @bp.route('/blog')
 def index():
+	db = get_db()
+	posts = db.execute(
+		'SELECT id, title, body, created'
+		'	FROM post'
+		'	ORDER BY created DESC'
+	).fetchall()
 	return render_template('blog/index.html')

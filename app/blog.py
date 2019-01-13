595a0cdc270be28454a5773e5d4a162aac6c380f
mark-solo <clueless.magician@gmail.com>
2019-01-13 13:42:27 +0300

    view post

--- app/blog.py
+++ app/blog.py
@@ -1,6 +1,7 @@
 from flask import (
 	Blueprint, flash, redirect, render_template, request, url_for
 )
+from werkzeug.exceptions import abort
 
 from app.db import get_db
 
@@ -16,7 +17,7 @@ def index():
 	).fetchall()
 	return render_template('blog/index.html', posts=posts)
 	
-@bp.route('/create', methods=('GET', 'POST'))
+@bp.route('/blog/create', methods=('GET', 'POST'))
 def create():
 	if request.method == 'POST':
 		title = request.form['title']
@@ -39,3 +40,54 @@ def create():
 			return redirect(url_for('blog.index'))
 		
 	return render_template('blog/create.html')
+	
+def get_post(id):
+	post = get_db().execute(
+		'SELECT id, title, body, created'
+		'	FROM post'
+		'	WHERE id = ?',
+		(id,)
+	).fetchone()
+	
+	if post is None:
+		abort(404, "Post id {0} doesn't exist.".format(id))
+		
+	return post
+	
+@bp.route('/blog/<int:id>/update', methods=('GET', 'POST'))
+def update(id):
+	post = get_post(id)
+	
+	if request.method == 'POST':
+		title = request.form['title']
+		body = request.form['body']
+		error = None
+		
+		if not title:
+			error = 'Title is required.'
+		
+		if error is not None:
+			flash(error)
+		else:
+			db = get_db()
+			db.execute(
+				'UPDATE post SET title = ?, body = ?'
+				'	WHERE id = ?',
+				(title, body, id)
+			)
+			db.commit()
+			return redirect(url_for('blog.index'))
+	return render_template('blog/update.html', post=post)
+	
+@bp.route('/blog/<int:id>/delete', methods=('POST',))
+def delete(id):
+	get_post(id)
+	db = get_db()
+	db.execute('DELETE FROM post WHERE id = ?', (id,))
+	db.commit()
+	return redirect(url_for('blog.index'))
+	
+@bp.route('/blog/<int:id>', methods=('GET', 'POST'))
+def view(id):
+	post = get_post(id)
+	return render_template('blog/view.html', post=post)
